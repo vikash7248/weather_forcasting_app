203a65d24a26a28e99bf0dc8d0d2b9e7adeb989a
Your Name <you@example.com>
2022-10-16 00:24:35 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "4268aecdedmshc20a4e175b2085dp1d05d5jsn1a77b83e5932",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "3fa7c77ea3e78b5337cd04b0a163693d"; // enter your key from openweather API
+  
